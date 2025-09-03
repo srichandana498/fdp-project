5caf8bdc2c0851b4194240361ab815be190e3a37
Srichandana Velagapudi <srichandanav12@gmail.com>
2025-09-04 01:36:55 +0530

    Fix Vite base for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fdp-project/",
   plugins: [react()],
-})
+  // base: '/',  // default, safe for Vercel/Netlify/Render
+});
+
